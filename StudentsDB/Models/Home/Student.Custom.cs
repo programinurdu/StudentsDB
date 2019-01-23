158436dac6d5bf1310e368e6838c69aa4a27d33a
programinurdu <aasdb@progd23.com>
2019-01-23 13:16:04 +0000

    Tutorial 5 - Validations are applied.

--- StudentsDB/Models/Home/Student.Custom.cs
+++ StudentsDB/Models/Home/Student.Custom.cs
@@ -13,11 +13,24 @@ namespace StudentsDB.Models.DB
 
     public class StudentMetaData
     {
+        [Display(Name = "Student Id:")]
         public int StudentId { get; set; }
+
+        [Display(Name = "Full Name:")]
+        [Required(ErrorMessage = "Full Name is required.")]
         public string FullName { get; set; }
+
+        [Display(Name = "Email:")]
+        [Required(ErrorMessage = "Email is required.")]
         public string Email { get; set; }
+
+        [Display(Name = "Mobile:")]
         public string Mobile { get; set; }
+
+        [Display(Name = "Telephone:")]
         public string Telephone { get; set; }
+
+        [Display(Name = "Notes:")]
         public string Notes { get; set; }
     }
 }
