b84dc5f02a52525b3a3f7ffd8a6e266240e5b41b
programinurdu <aasdb@progd23.com>
2019-01-23 13:05:08 +0000

    Tutorial 5 - MetaData Class Added

--- StudentsDB/Models/Home/Student.Custom.cs
+++ StudentsDB/Models/Home/Student.Custom.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace StudentsDB.Models.DB
 {
+    [MetadataType(typeof(StudentMetaData))]
     public partial class Student
     {
+    }
+
+    public class StudentMetaData
+    {
+        public int StudentId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Telephone { get; set; }
+        public string Notes { get; set; }
     }
 }
